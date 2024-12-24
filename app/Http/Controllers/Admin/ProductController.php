3faa670d2eaa72561e8d92d71b8cc9445f9931c2
Tigistu <tigistubegashaw1@gmail.com>
2024-12-24 15:23:16 +0300

    implement non admin users can not access the admin prefixed sites

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -5,16 +5,31 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
+    private function checkAdmin()
+    {
+        if (Auth::check() && Auth::user()->role !== 'admin') {
+            return redirect('/home');
+        }
+    }
     public function create()
     {
+        if ($redirect = $this->checkAdmin()) {
+            return $redirect;
+        }
+
         return view('admin.products.create');
     }
 
     public function store(Request $request)
     {
+        if ($redirect = $this->checkAdmin()) {
+            return $redirect;
+        }
+
         $request->validate([
             'name' => 'required',
             'price' => 'required|numeric',
@@ -36,6 +51,10 @@ class ProductController extends Controller
 
     public function index()
     {
+        if ($redirect = $this->checkAdmin()) {
+            return $redirect;
+        }
+
         $products = Product::all();
         return view('admin.products.index', compact('products'));
     }
