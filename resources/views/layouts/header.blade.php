b6dfb4ba719cda684a556ced838af19fe9fbe6b7
adem <adem13617@gmail.com>
2025-01-29 11:28:20 +0300

    remove unwanted features

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -76,32 +76,12 @@
                 <ul>
                     <li><a href="{{ url('/home') }}">Home</a></li>
                     <li class="active"><a href="{{ url('/shop') }}">Shop</a></li>
-                    <li><a href="{{ url('/product-detail') }}">Product</a></li>
                     <li><a href="{{ url('/cart') }}">Cart <span class="cart-item-count">{{ $cartItemCount }}</span></a></li>
                     <li><a href="{{ url('/checkout') }}">Checkout</a></li>
                 </ul>
             </nav>
 
-            <!-- Button Group -->
-            <div class="amado-btn-group mt-30 mb-100">
-                <a href="#" class="btn amado-btn mb-15">%Discount%</a>
-                <a href="#" class="btn amado-btn active">New this week</a>
-            </div>
 
-            <!-- Cart Menu -->
-            <div class="cart-fav-search mb-100">
-                <a href="{{ url('/cart') }}" class="cart-nav"><img src="{{ asset('img/core-img/cart.png') }}" alt=""> Cart <span>(0)</span></a>
-                <a href="#" class="fav-nav"><img src="{{ asset('img/core-img/favorites.png') }}" alt=""> Favourite</a>
-                <a href="#" class="search-nav"><img src="{{ asset('img/core-img/search.png') }}" alt=""> Search</a>
-            </div>
-
-            <!-- Social Button -->
-            <div class="social-info d-flex justify-content-between">
-                <a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
-                <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-            </div>
         </header>
         <!-- Header Area End -->
 
